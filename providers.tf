f7789641f86ec7d1818a67a16c3fcae038ac304f
ROHAN SINGH <sron243@gmail.com>
2021-11-03 19:36:55 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,15 +1,6 @@
 provider "aws" {
   region = "eu-central-1"
 }
-
-terraform {
-  backend "s3" {
-    bucket = "rohan-terraform-data"
-    key    = "test"
-    region = "eu-central-1"
-  }
-}
-
 data "aws_region" "current" {}
 
 data "aws_availability_zones" "available" {}
